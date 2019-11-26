21e9e9035e133e4873ae294dc8e0e9f86d6118d6
Jesse Hills <jesse@springload.co.nz>
2019-11-26 22:50:25 +1300

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='python-juicenet',
-      version='0.1.6',
+      version='0.1.7',
       description='Read and control Juicenet/Juicepoint/Juicebox based EVSE devices',
       url='http://github.com/jesserockz/python-juicenet',
       author='Jesse Hills',
