c3a9a3a5f6817fc741156cb071241861de53dd0e
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2021-05-12 10:50:12 +1200

    Remove hard dependency on asyncio (#12)

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import find_packages, setup
 
 setup(name='python-juicenet',
-      version='1.0.1',
+      version='1.0.2',
       description='Read and control Juicenet/Juicepoint/Juicebox based EVSE devices',
       url='http://github.com/jesserockz/python-juicenet',
       author='@jesserockz',
       license='MIT',
       install_requires=[
             'aiohttp',
-            'asyncio'
       ],
       packages=find_packages(exclude=["dist"]),
       zip_safe=True)
